67c6a4ef1f1a1f1fc422f6965484b6489d8f2fe4
lucaslarry <lucasslarryy@gmail.com>
2025-03-13 19:09:36 -0300

    Add get,delete e put

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,8 +7,25 @@ class UserController {
     return user;
   }
 
-  static async getUserByEmail(email) {
-    return await UserModel.findOne({ email });
+  static async getUsers() {
+    return await UserModel.find();
+  }
+
+  static async getUserById(id) {
+    return await UserModel.findById(id);
+  }
+
+  static async updateUser(id, { name, email, password }) {
+    const updateData = {};
+    if (name) updateData.name = name;
+    if (email) updateData.email = email;
+    if (password) updateData.password = password;
+    
+    return await UserModel.findByIdAndUpdate(id, updateData, { new: true });
+  }
+
+  static async deleteUser(id) {
+    return await UserModel.findByIdAndDelete(id);
   }
 }
 
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -7,6 +7,12 @@ const resolvers = {
       if (!context.user) throw new Error('Não autenticado');
       return await UserController.getUserByEmail(context.user.email);
     },
+    users: async () => {
+      return await UserController.getUsers();
+    },
+    user: async (parent, { id }) => {
+      return await UserController.getUserById(id);
+    },
   },
   Mutation: {
     register: async (parent, { name, email, password }) => {
@@ -14,6 +20,12 @@ const resolvers = {
     },
     login: async (parent, { email, password }) => {
       return await AuthController.login({ email, password });
+    },
+    updateUser: async (parent, { id, name, email, password }) => {
+      return await UserController.updateUser(id, { name, email, password });
+    },
+    deleteUser: async (parent, { id }) => {
+      return await UserController.deleteUser(id);
     },
   },
 };
